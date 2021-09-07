89278c4527eaf983112ab810f9fa836fa6ac62c6
Sanier <Sanier20@mail.ru>
2021-09-07 19:32:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практикум_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLenght, bool HasPet, string[] favcolors, double Age) User;
+        }
+    }
+}
