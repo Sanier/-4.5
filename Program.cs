acc35a36c76cb3de1a519c1161b3c6e2be25e221
Sanier <Sanier20@mail.ru>
2021-09-07 19:39:33 +0300

    4.5.2 Заполнение половины кортежа

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace Практикум_4._5
         static void Main(string[] args)
         {
             (string Name, string LastName, string Login, int LoginLenght, bool HasPet, string[] favcolors, double Age) User;
+
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            User.LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
         }
     }
 }
