e9e6770154e30ad1189d835f94025fce41f5f668
Sanier <Sanier20@mail.ru>
2021-09-07 19:50:40 +0300

    4.5.6 Добавлен цикл для 3 пользователей

--- Program.cs
+++ Program.cs
@@ -6,40 +6,43 @@ namespace Практикум_4._5
     {
         static void Main(string[] args)
         {
+            
             (string Name, string LastName, string Login, int LoginLenght, bool HasPet, string[] favcolors, double Age) User;
 
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-            User.LoginLenght = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-            var result = Console.ReadLine();
-            if (result == "Да")
+            for (int k = 0; k < 3; k++)
             {
-                User.HasPet = true;
-            }
-            else
-            {
-                User.HasPet = false;
-            }
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
 
-            Console.WriteLine("Введите возраст пользователя");
-            User.Age = double.Parse(Console.ReadLine());
-            
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
 
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLenght = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var result = Console.ReadLine();
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
             }
-
         }
     }
 }
